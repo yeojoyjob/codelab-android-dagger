6aeccfdaec792db8df5fd5d310e6cecdfcadf425
yeojoy <yeojoy.me@gmail.com>
2023-10-30 23:19:13 -0400

    Finish: 4th step No. 7

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,11 +1,22 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
-// Definition of a Dagger Component
-@Component
+// Definition of a Dagger Component that adds info from StorageModule to the graph
+@Component(modules = [StorageModule::class])
 interface AppComponent {
+
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindsInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+
     // Classes that can be injected by this Component
     fun inject(activity: RegistrationActivity)
 }
